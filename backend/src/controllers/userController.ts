166444da9643eaedc1234cefc8af2cb5a5402472
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-14 16:47:12 +0300

    add changes

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -13,6 +13,7 @@ interface RegisterRequestBody {
   role: string;
 }
 
+// normal user registration
 export const userRegister = async (
   req: Request<{}, {}, RegisterRequestBody>,
   res: Response
@@ -86,6 +87,8 @@ export const userRegister = async (
     });
   }
 };
+
+// admin registration
 export const adminRegister = async (
   req: Request<{}, {}, RegisterRequestBody>,
   res: Response
@@ -160,6 +163,82 @@ export const adminRegister = async (
   }
 };
 
+// staff registration
+export const staffRegister = async (
+  req: Request<{}, {}, RegisterRequestBody>,
+  res: Response
+) => {
+  try {
+    const { fullname, email, username, password, role, blood_type } = req.body;
+    if (!fullname || !email || !username || !password || !blood_type) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "fullname, email, username, password, blood_type these fields are required",
+      });
+    }
+    const userExists = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: email }, { username: username }],
+      },
+    });
+
+    if (userExists) {
+      let message = "username or email already exists";
+      if (userExists.email === email) {
+        message = "user with this email already exists";
+      } else if (userExists.username === username) {
+        message = "Username already taken";
+      }
+      return res.status(400).json({
+        success: false,
+        message,
+      });
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullname,
+        email,
+        username,
+        password: hashedPassword,
+        blood_type: blood_type as $Enums.BloodType,
+        role: "STAFF",
+      },
+      select: {
+        id: true,
+        fullname: true,
+        email: true,
+        username: true,
+        blood_type: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    const expiresIn = 7 * 24 * 60 * 60;
+
+    const token = generateToken(newUser.id, expiresIn);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      newUser,
+      token,
+    });
+  } catch (error: any) {
+    console.error("Registration error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// login
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
@@ -209,6 +288,7 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 
+// get user profile
 export const profile = async (req: Request, res: Response) => {
   try {
     res.status(200).json({
