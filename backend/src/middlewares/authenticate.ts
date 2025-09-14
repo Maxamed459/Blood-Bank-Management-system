216aa2e9f3444097afa9fae32cb736dd4c293272
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-14 16:45:19 +0300

    add authenticate middleware

---
+++ backend/src/middlewares/authenticate.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma";
+import { JWT_SECRET } from "../config/config";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
+
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Access denied. No token provided. please login to get a token",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: (decoded as { userId: string }).userId,
+      },
+      select: {
+        id: true,
+        fullname: true,
+        email: true,
+        username: true,
+        blood_type: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token - user not found",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error: any) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid authentication token" });
+  }
+};
