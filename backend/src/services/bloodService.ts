049b39679802c83e6e08cfec7a66009ebc25991e
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-14 16:49:47 +0300

    added blood services

--- backend/src/services/bloodService.ts
+++ backend/src/services/bloodService.ts
@@ -1,1 +1,66 @@
-import prisma from "../generated/prisma";
+import prisma from "../lib/prisma";
+import { $Enums } from "../generated/prisma";
+
+// creating blood
+export async function addingBlood(
+  type: string,
+  quantity: number,
+  donorId: string
+) {
+  return await prisma.blood.create({
+    data: {
+      type: type as $Enums.BloodType,
+      quantity,
+      donor: { connect: { id: donorId } },
+    },
+  });
+}
+
+// getting all blood
+export async function gettingAllBlood() {
+  return await prisma.blood.findMany();
+}
+
+// getting blood by id
+export async function gettingBloodById(id: string) {
+  return await prisma.blood.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+// updating blood
+export async function updatingBlood(
+  id: string,
+  type: string,
+  quantity: number
+) {
+  return await prisma.blood.update({
+    where: {
+      id,
+    },
+    data: {
+      type: type as $Enums.BloodType,
+      quantity,
+    },
+  });
+}
+
+// deleting blood
+export async function deletingBlood(id: string) {
+  return await prisma.blood.delete({
+    where: {
+      id,
+    },
+  });
+}
+
+// getting blood by type
+export async function gettingBloodByType(type: string) {
+  return await prisma.blood.findMany({
+    where: {
+      type: type as $Enums.BloodType,
+    },
+  });
+}
