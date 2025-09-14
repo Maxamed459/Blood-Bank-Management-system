1b52516e5b9a28a3290a432ba65ab58bcbb42101
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-14 16:46:37 +0300

    add changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userRoute } from "./routes/userRoute";
 import cors from "cors";
+import { PORT } from "./config/config";
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -25,7 +26,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/auth", userRoute);
 
-app.use("*", (req: Request, res: Response) => {
+app.use((req: Request, res: Response) => {
   res.status(404).json({
     message: "Route not found",
     error: "The requested endpoint does not exist",
@@ -40,8 +41,6 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     error: process.env.NODE_ENV === "development" ? err.message : {},
   });
 });
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀server is running on http://localhost:${PORT}`);
