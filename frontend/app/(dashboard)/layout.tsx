f6ff8bfd93fc2098c9802eaf10f390ef11de7e4c
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-15 23:09:44 +0300

    add dashbaord

---
+++ frontend/app/(dashboard)/layout.tsx
@@ -0,0 +1,42 @@
+"use client";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Provider } from "react-redux";
+import store from "@/store";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider store={store}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Provider>
+      </body>
+    </html>
+  );
+}
