ac4ba0bad7c65d64f172bc2079bdd148dc57ffd1
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-15 23:11:56 +0300

    add types folder

---
+++ frontend/types/types.ts
@@ -0,0 +1,47 @@
+export type BloodType = {
+  A_POSITIVE: string;
+  A_NEGATIVE: string;
+  B_POSITIVE: string;
+  B_NEGATIVE: string;
+  O_POSITIVE: string;
+  O_NEGATIVE: string;
+  AB_POSITIVE: string;
+  AB_NEGATIVE: string;
+};
+export type Gender = {
+  MALE: string;
+  FEMALE: string;
+};
+export type Role = {
+  USER: string;
+  ADMIN: string;
+  STAFF: string;
+};
+
+export interface User {
+  id: string;
+  fullname: string;
+  email: string;
+  username: string;
+  blood_type: BloodType;
+  gender: Gender;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Types
+export interface AuthState {
+  user: {
+    id: string;
+    fullname: string;
+    email: string;
+    username: string;
+    role: Role;
+    gender: Gender;
+    blood_type: BloodType;
+    token: string;
+  } | null;
+  loading: boolean;
+  error: string | null;
+}
