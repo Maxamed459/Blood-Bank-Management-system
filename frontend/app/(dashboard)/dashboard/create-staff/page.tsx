c2749cd4a9ed21c6237c4eba6b012da33b94184b
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-20 11:49:58 +0300

    add create staff page

---
+++ frontend/app/(dashboard)/dashboard/create-staff/page.tsx
@@ -0,0 +1,10 @@
+import RegisterStaff from "../../_components/RegisterStaff";
+
+export const metadata = {
+  title: "Create Staff Account",
+  description: "Register a new staff account for dashboard access.",
+};
+
+export default function createAdminAccount() {
+  return <RegisterStaff />;
+}
