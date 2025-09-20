5e26e354619243f2d325c895c7fd2b58f55287f0
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-20 11:49:41 +0300

    add create admin page

---
+++ frontend/app/(dashboard)/dashboard/create-admin/page.tsx
@@ -0,0 +1,10 @@
+import RegisterAdmin from "../../_components/RegisterAdmin";
+
+export const metadata = {
+  title: "Create Admin Account",
+  description: "Register a new admin account for dashboard access.",
+};
+
+export default function createAdminAccount() {
+  return <RegisterAdmin />;
+}
