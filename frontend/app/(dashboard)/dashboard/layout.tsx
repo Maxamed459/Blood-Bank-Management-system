12cd7222853e6479d6f6e8d057fbe329287c9aac
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-20 11:51:08 +0300

    add dashboardLayout page

---
+++ frontend/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../../globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Provider } from "react-redux";
+import store from "@/store";
+import { ToasterProvider } from "@/components/providers/toester-provider";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider store={store}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SidebarProvider
+              style={
+                {
+                  "--sidebar-width": "calc(var(--spacing) * 72)",
+                  "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+              }
+            >
+              <AppSidebar variant="inset" />
+              <SidebarInset>
+                <SiteHeader />
+                {children}
+              </SidebarInset>
+            </SidebarProvider>
+            <ToasterProvider />
+          </ThemeProvider>
+        </Provider>
+      </body>
+    </html>
+  );
+}
