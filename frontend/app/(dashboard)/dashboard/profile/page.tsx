06b2209108c90f4921d5c9e5fc8691cd15956d2f
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-20 11:50:47 +0300

    add profile page

---
+++ frontend/app/(dashboard)/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+import ProfileForm from "../../_components/ProfileForm";
+
+export default async function ProfilePage() {
+  return (
+    <div className="p-4">
+      <div className="flex items-center justify-between">
+        <ProfileForm />
+      </div>
+    </div>
+  );
+}
