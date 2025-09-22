3145b312e42fc21a55edf81264e24ddad0802b51
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-22 17:30:44 +0300

    added blood request blood page

---
+++ frontend/app/(dashboard)/dashboard/request-blood/page.tsx
@@ -0,0 +1,9 @@
+import RequestForm from "../../_components/RequestForm";
+
+export default function RequestBlood() {
+  return (
+    <div className="p-4">
+      <RequestForm />
+    </div>
+  );
+}
