0d03540af48f1111bcae28b2c12d877dad8a5df2
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-22 17:30:01 +0300

    added blood record page

---
+++ frontend/app/(dashboard)/dashboard/blood-record/page.tsx
@@ -0,0 +1,9 @@
+import AddBloodRecord from "../../_components/AddBloodRecord";
+
+export default function BloodRecord() {
+  return (
+    <div className="p-4">
+      <AddBloodRecord />
+    </div>
+  );
+}
