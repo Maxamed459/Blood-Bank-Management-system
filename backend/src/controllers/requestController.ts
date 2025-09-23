b4dec247587f74955999434d6f6747e3b2f1269e
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-23 07:59:42 +0300

    add requestController

---
+++ backend/src/controllers/requestController.ts
@@ -0,0 +1,150 @@
+import { Request, Response } from "express";
+import {
+  addingRequest,
+  deletingRequest,
+  gettingRequest,
+  gettingRequestByBloodType,
+} from "../services/requestService";
+import prisma from "../lib/prisma";
+// add request
+export const addRequest = async (req: Request, res: Response) => {
+  try {
+    const { blood_type, quantity_needed, hospital } = req.body;
+    const requester_id = req.user?.id;
+    // checking if missing required fields
+    if (!blood_type || !quantity_needed || !hospital) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "blood_type & quantity_needed & hospital these fields are required",
+      });
+    }
+    // checking if the quantity_needed is valid number
+    if (quantity_needed <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter valid quantity",
+      });
+    }
+    // setting status to PENDING if the request has sent a user or admin
+    let status;
+    switch (req.user?.role) {
+      case "ADMIN":
+      case "STAFF":
+        status = "APPROVED";
+        break;
+      case "USER":
+        status = "PENDING";
+        break;
+      default:
+        status = "REJECTED"; // fallback if role is missing or invalid
+    }
+
+    const newRequest = await addingRequest(
+      requester_id,
+      blood_type,
+      quantity_needed,
+      hospital,
+      status
+    );
+    res.status(201).json({
+      success: true,
+      message: "request sent successfully",
+      data: newRequest,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// get all request
+export const getAllRequest = async (req: Request, res: Response) => {
+  try {
+    const allRequest = await gettingRequest();
+    if (allRequest.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "no request founded",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "here are the all request",
+      data: allRequest,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// get request by blood type
+export const getRequestByBloodType = async (req: Request, res: Response) => {
+  try {
+    const { type } = req.params;
+    const Request = await gettingRequestByBloodType(type);
+    if (Request.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "no request founded by this blood type",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "here are the all request for this blood type",
+      data: Request,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// update request
+export const updateRequest = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { blood_type, quantity_needed, hospital, status } = req.body;
+    const updatedRequest = await prisma.request.update({
+      where: { id },
+      data: {
+        blood_type,
+        quantity_needed,
+        hospital,
+        status,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "request updated successfully",
+      data: updatedRequest,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// delete request
+export const deleteRequest = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await deletingRequest(id);
+    res.status(200).json({
+      success: true,
+      message: "request deleted successfully",
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
