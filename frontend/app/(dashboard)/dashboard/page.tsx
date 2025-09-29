c7526453cd4269685f11b3efb1c45aaa1a2748a3
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-29 18:47:25 +0300

    updated page

--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
 
     fetchBlood();
     fetchRequests();
-  }, [dispatch]);
+  }, [dispatch, requests]);
 
   useEffect(() => {
     if (!loading && !user) {
@@ -220,92 +220,90 @@ export default function Page() {
           </div>
         </div>
       )}
-      {user?.role === "USER" && (
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="w-full px-4">
-              <h2 className="px-4 py-2 text-xl font-bold">
-                Blood Requests please donate if you have those blood type
-              </h2>
-              <div className="overflow-auto rounded-md border">
-                <table className="min-w-[950px] divide-y divide-gray-200 dark:divide-gray-700 bg-white dark:bg-gray-900">
-                  <thead>
-                    <tr>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        Requester Id
-                      </th>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        Blood type
-                      </th>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        Quantity needed
-                      </th>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        Hospital
-                      </th>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        Status
-                      </th>
-                      <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
-                        CreatedAt
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                    {requests && requests.length > 0 ? (
-                      requests
-                        .filter((request) => request.status === "APPROVED") // keep only pending
-                        .map((request, index) => (
-                          <tr
-                            key={index}
-                            className="hover:bg-gray-100 dark:hover:bg-gray-800"
-                          >
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              {request.requester_id}
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              {request.blood_type}
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              {request.quantity_needed} L
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              {request.hospital}
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              <span
-                                className={`px-2 py-1 text-xs font-medium rounded-full ${
-                                  request.status === "PENDING"
-                                    ? "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-200"
-                                    : request.status === "APPROVED"
-                                    ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200"
-                                    : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200"
-                                }`}
-                              >
-                                {request.status}
-                              </span>
-                            </td>
-                            <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
-                              {formatDataTime(request.createdAt)}
-                            </td>
-                          </tr>
-                        ))
-                    ) : (
-                      <tr>
-                        {[...Array(6)].map((_, i) => (
-                          <td key={i} className="px-4 py-2 whitespace-nowrap">
-                            <div className="h-4 bg-gray-300 rounded animate-pulse dark:bg-gray-700"></div>
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="w-full px-4">
+            <h2 className="px-4 py-2 text-xl font-bold">
+              Blood Requests please donate if you have those blood type
+            </h2>
+            <div className="overflow-auto rounded-md border">
+              <table className="min-w-[950px] divide-y divide-gray-200 dark:divide-gray-700 bg-white dark:bg-gray-900">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      Requester Id
+                    </th>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      Blood type
+                    </th>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      Quantity needed
+                    </th>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      Hospital
+                    </th>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      Status
+                    </th>
+                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase">
+                      CreatedAt
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                  {requests && requests.length > 0 ? (
+                    requests
+                      .filter((request) => request.status === "APPROVED") // keep only pending
+                      .map((request, index) => (
+                        <tr
+                          key={index}
+                          className="hover:bg-gray-100 dark:hover:bg-gray-800"
+                        >
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            {request.requester_id}
                           </td>
-                        ))}
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            {request.blood_type}
+                          </td>
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            {request.quantity_needed} L
+                          </td>
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            {request.hospital}
+                          </td>
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            <span
+                              className={`px-2 py-1 text-xs font-medium rounded-full ${
+                                request.status === "PENDING"
+                                  ? "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-200"
+                                  : request.status === "APPROVED"
+                                  ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200"
+                                  : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200"
+                              }`}
+                            >
+                              {request.status}
+                            </span>
+                          </td>
+                          <td className="px-4 py-2 whitespace-nowrap text-gray-700 dark:text-gray-200">
+                            {formatDataTime(request.createdAt)}
+                          </td>
+                        </tr>
+                      ))
+                  ) : (
+                    <tr>
+                      {[...Array(6)].map((_, i) => (
+                        <td key={i} className="px-4 py-2 whitespace-nowrap">
+                          <div className="h-4 bg-gray-300 rounded animate-pulse dark:bg-gray-700"></div>
+                        </td>
+                      ))}
+                    </tr>
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
