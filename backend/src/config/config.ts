df2f794b3fe5f0ec5ccd421a0ac30e11a6462af0
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-30 10:21:36 +0300

    add OAuth2 required fields

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -7,3 +7,7 @@ export const JWT_SECRET = process.env.JWT_SECRET || "bloodbanksecret_12345";
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const EMAIL = process.env.EMAIL;
 export const PASSWORD = process.env.PASSWORD;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const REDIRECT_URI = process.env.REDIRECT_URI;
+export const OAUTH_REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN;
