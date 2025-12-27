ab3e0bd4c71082067458dacffb899f18964f4113
Maxamed459 <maxamedmahdi459@gmail.com>
2025-12-27 22:19:16 +0300

    updated the ui of dashboard header

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -1,8 +1,11 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ModeToggle } from "./ui/mode-toggle";
+import { useAppSelector } from "@/store";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export function SiteHeader() {
+  const {user} = useAppSelector((state) => state.auth);
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -11,8 +14,24 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="text-xl font-medium">Hi <span className="font-bold text-red-600">{user?.role.toLowerCase()}, {user?.fullname.toLowerCase()}</span> Welcome to the Blood Bank Management System</h1>
         <div className="ml-auto flex items-center gap-2">
+          <div className="W-12 H-12 rounded-full bg-gray-100 dark:bg-gray-600 border p-1">
+            {user?.gender === "MALE" ? (
+              <>
+              <img src="/man_avater.svg" className="w-6" alt="man avater" />
+              </>
+            ) : (
+              <>
+              <img src="/woman_avater.svg" className="w-6" alt="woman avater" />
+              </>
+            )}
+            
+          </div>
+          <div>
+              <p className="text-xs">{user?.fullname}</p>
+              <p className="text-xs">{user?.email}</p>
+            </div>
           <ModeToggle />
         </div>
       </div>
