f87db4e3c8904cc9b160a331233e49c7c2d9a311
Maxamed459 <maxamedmahdi459@gmail.com>
2026-01-23 18:08:33 +0300

    updated the ui

--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -11,8 +11,23 @@ import { getAllRequests } from "@/store/slices/requestSlice";
 import { BloodTable } from "./_components/BloodTable";
 import { ApproveRequest } from "./_components/ApproveRequest";
 import { BloodRequests } from "./_components/BloodRequests";
-import { BarChartBlood } from "./_components/BarChartBlood";
-import { PieChartBlood } from "./_components/PieChartBlood";
+import dynamic from "next/dynamic";
+
+const LineChartComponent = dynamic(
+  () => import("@/components/chart/LineChart"),
+  { ssr: false },
+);
+
+const data = [
+  { name: "A+", value: 30 },
+  { name: "A-", value: 45 },
+  { name: "B+", value: 28 },
+  { name: "B-", value: 50 },
+  { name: "AB+", value: 20 },
+  { name: "AB-", value: 60 },
+  { name: "O+", value: 50 },
+  { name: "O-", value: 80 },
+];
 
 export default function Page() {
   const { user, loading, token } = useAppSelector((state) => state.auth);
@@ -35,11 +50,14 @@ export default function Page() {
       {(user?.role === "ADMIN" || user?.role === "STAFF") && (
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-4">
               <SectionCards />
-              <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 max-w-[95%] min-w-full mx-auto p-4 ">
-                <BarChartBlood />
-                <PieChartBlood />
+              <div className="flex flex-col px-4 space-y-5">
+                <div>
+                  <h2 className="text-xl font-semibold">Blood type</h2>
+                  <p className="text-sm">The weekly range of blood types</p>
+                </div>
+                <LineChartComponent data={data} />
               </div>
               <ApproveRequest />
               <BloodTable />
