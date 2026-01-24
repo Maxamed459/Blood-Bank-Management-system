0b7d4e9c6371ed68d81a0f595056947f839ef1c3
Maxamed459 <maxamedmahdi459@gmail.com>
2026-01-24 10:04:57 +0300

    updated the fetching profile data using tanStack query for fetching and caching 1 minute

--- frontend/app/(dashboard)/dashboard/_components/ProfileForm.tsx
+++ frontend/app/(dashboard)/dashboard/_components/ProfileForm.tsx
@@ -8,29 +8,32 @@ import toast from "react-hot-toast";
 import { useEffect } from "react";
 import { Pencil, User } from "lucide-react";
 import { SlUserFemale } from "react-icons/sl";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ProfileForm() {
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.auth);
 
-  useEffect(() => {
-    const Profile = async () => {
-      try {
-        // dispatch(profile()) returns a Promise
-        const profilePromise = dispatch(profile()).unwrap();
+  const Profile = async () => {
+    try {
+      // dispatch(profile()) returns a Promise
+      const profilePromise = dispatch(profile()).unwrap();
 
-        await toast.promise(profilePromise, {
-          loading: "Getting your profile...",
-          success: (data) => <b>Welcome {data.username}!</b>,
-          error: (err) => <b>{err || "Failed to load profile"}</b>,
-        });
-      } catch (err) {
-        console.error("Profile fetch failed:", err);
-      }
-    };
+      await toast.promise(profilePromise, {
+        loading: "Getting your profile...",
+        success: (data) => <b>Welcome {data.username}!</b>,
+        error: (err) => <b>{err || "Failed to load profile"}</b>,
+      });
+    } catch (err) {
+      console.error("Profile fetch failed:", err);
+    }
+  };
 
-    Profile();
-  }, [dispatch]);
+  const { data, isPending, error } = useQuery({
+    queryKey: ["profile"],
+    queryFn: Profile,
+    staleTime: 60000, // 1 minute
+  });
 
   return (
     <div className="w-full">
@@ -39,26 +42,26 @@ export default function ProfileForm() {
       </div>
 
       <div className="w-full md:w-[85%] mx-auto flex flex-col lg:flex-row mt-15 gap-6">
-        <div className="w-full lg:w-1/2 border-1 shadow-md p-4 rounded-md">
+        <div className="w-full lg:w-1/2 border shadow-md p-4 rounded-md">
           {user?.gender === "MALE" && <User className="w-24 h-24" />}
           {user?.gender === "FEMALE" && <SlUserFemale className="w-24 h-24" />}
 
           <div className="mt-4">
             <div className="mt-4">
-              <p className="border-b-1 py-1 text-muted-foreground">
+              <p className="border-b py-1 text-muted-foreground">
                 <b>Fullname: </b>
                 {user?.fullname}
               </p>
-              <p className="border-b-1 py-1 text-muted-foreground">
+              <p className="border-b py-1 text-muted-foreground">
                 <b>Email: </b>
                 {user?.email}
               </p>
-              <p className="border-b-1 py-1 text-muted-foreground">
+              <p className="border-b py-1 text-muted-foreground">
                 {" "}
                 <b>Id: </b>
                 {user?.id}
               </p>
-              <p className="border-b-1 py-1 text-muted-foreground">
+              <p className="border-b py-1 text-muted-foreground">
                 <b>Account created: </b>
                 {formatDataTime(user?.createdAt)}
               </p>
@@ -66,7 +69,7 @@ export default function ProfileForm() {
           </div>
         </div>
         <div className="w-full lg:w-1/2 grid gap-4">
-          <div className="border-1 shadow-md p-4 rounded-md border-t-4 border-t-blue-600">
+          <div className="border shadow-md p-4 rounded-md border-t-4 border-t-blue-600">
             <h2 className="text-xl font-semibold">Role</h2>
             <div className="mt-4">
               <p className="text-muted-foreground">
@@ -84,10 +87,10 @@ export default function ProfileForm() {
               </p>
             </div>
           </div>
-          <div className="border-1 shadow-md p-4 rounded-md border-t-4 border-t-[#E81C24]">
+          <div className="border shadow-md p-4 rounded-md border-t-4 border-t-[#E81C24]">
             <h2 className="text-xl font-semibold">Blood Type</h2>
             <div className="">
-              <p className="border-b-1 py-1 text-muted-foreground">
+              <p className="border-b py-1 text-muted-foreground">
                 <b>Blood type: </b>
                 {user?.blood_type}
               </p>
